2763ec4f85d81fb654a0bffcbd08138c25393fb8
brad <brad.vatne@gmail.com>
2023-05-31 10:12:56 -0400

    more work on binary tree

--- src/app/data_structures/tree.ts
+++ src/app/data_structures/tree.ts
@@ -17,16 +17,38 @@ class BasicUnbalancedTree<T> {
 		this.root = null;
 	}
 
-	insert(value: T, node?: TreeNode<T>) {
-		if (!node) {
-			if (!this.root) {
-				this.root = new TreeNode(value)
-			}
+	insert(node: TreeNode<T> | null, value: T) {
+		if (node === null) {
+			return new TreeNode(value);
+		} else if (value < node.value) {
+			node.left = this.insert(node.left, value);
+		} else if (value > node.value) {
+			node.right = this.insert(node.right, value);
 		}
-		if (!this.root) {
-			if (!node) {
-				this.root.left > 
-			}
+
+		return node;
+	}
+
+	find(node: TreeNode<T> | null, value: T): boolean {
+		if (node === null) {
+			return false;
+		} else if (node.value === value) {
+			return true;
+		} else if (value < node.value) {
+			this.find(node.left, value);
+		} else if (value > node.value) {
+			this.find(node.right, value);
 		}
+
+		return false;
+	}
+
+	delete(node: TreeNode<T> | null, value: T): TreeNode<T> | null {
+		if (node === null) {
+			return null;
+		} else if (node.value === value) {
+			
+		}
+		return node;
 	}
 }
